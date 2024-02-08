6ba15dc56c509ca00e1e0e7ac1d395b66bcd569f
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-02-08 12:14:43 +0530

    Add verification token functionality

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -3,12 +3,11 @@ import { db } from "@/lib/db";
 export const getVerificationTokenByEmail = async (email: string) => {
   try {
     const verificationToken = await db.verificationToken.findFirst({
-      where: {
-        email,
-      },
+      where: { email },
     });
+
     return verificationToken;
-  } catch (error) {
+  } catch {
     return null;
   }
 };

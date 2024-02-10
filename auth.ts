d05794711f54aab6b6a98d82cdb25e84bac00975
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-02-10 09:25:33 +0530

    Add support for two-factor authentication

--- auth.ts
+++ auth.ts
@@ -60,6 +60,9 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
 
       return session;
     },
@@ -69,6 +72,7 @@ export const {
       const existingUser = await getUserByID(token.sub!);
       if (!existingUser) return token;
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
--- components/auth/user-info.tsx
+++ components/auth/user-info.tsx
@@ -1,5 +1,6 @@
 import { ExtendedUser } from "@/next-auth";
 import { Card, CardContent, CardHeader } from "../ui/card";
+import { Badge } from "../ui/badge";
 
 interface UserInfoProps {
   user?: ExtendedUser;
@@ -39,9 +40,9 @@ export const UserInfo = ({ user, label }: UserInfoProps) => {
         </div>
         <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className=" text-sm font-medium">Two Factor Authentication</p>
-          <p className=" truncate text-xs max-w-[180px] font-mono bg-slate-100 rounded-sm p-1">
-            OFF
-          </p>
+          <Badge variant={user?.isTwoFactorEnabled ? "success" : "destructive"}>
+            {user?.isTwoFactorEnabled ? "ON" : "OFF"}
+          </Badge>
         </div>
       </CardContent>
     </Card>

5140b97c1562c9b87f18647e1cbfe85cb1344ab6
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-02-12 13:55:25 +0530

    Add callbackUrl support in Social component and use domain variable in mail module

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -5,11 +5,14 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
 import { signIn } from "next-auth/react";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
 
 export const Social = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
   const signInHandler = (provider: "github" | "google") => {
     signIn(provider, {
-      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
   };
 
